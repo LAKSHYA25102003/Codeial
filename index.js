09bf3ca8d6073ce15bb5fb5eeb7943063a871e21
Lakshya <llakshya63gmail.com>
2022-04-03 14:56:23 +0530

    First Commit:whole setup and directory with express server setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const app=express();
+const port=8000;
+
+
+
+
+app.listen(port,function(err){
+    if(err)
+    {
+        console.log(`Error:${err}`);
+        return ;
+    }
+    console.log(`Server is sarted successfully on the port:${port}`);
+});
